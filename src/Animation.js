b5aa0dabc2b5ddb3f745632bca7834dee1015a74
Deborah Ong <deborah.oyt@gmail.com>
2021-10-11 22:49:53 -0700

    cheese animation

---
+++ src/Animation.js
@@ -0,0 +1,46 @@
+import { Component } from 'react'
+import { Keyframes, Frame } from 'react-keyframes'
+import C1 from './assets/Cheese3.png'
+import C2 from './assets/Cheese5.png'
+import C3 from './assets/Cheese6.png'
+import C4 from './assets/Cheese8.png'
+import C5 from './assets/Cheese10.png'
+import C6 from './assets/Cheese16.png'
+import C7 from './assets/Cheese20.png'
+
+class Animation extends Component {
+  render () {
+    return <Keyframes component="pre" loop={true} className="cheeseimg">
+      <Frame duration={2500} >
+        <img src={C1} alt="1"/>    
+      </Frame>
+      <Frame duration={20} >
+      <img src={C2} alt="2"/>       
+      </Frame>
+      <Frame duration={20} >
+      <img src={C3} alt="3"/>       
+      </Frame>
+      <Frame duration={20} >
+      <img src={C4} alt="4"/>       
+      </Frame>
+      <Frame duration={20} >
+      <img src={C5} alt="5"/>       
+      </Frame>
+      <Frame duration={15} >
+      <img src={C6} alt="6"/>       
+      </Frame>
+      <Frame duration={2000} >
+      <img src={C7} alt="7"/>       
+      </Frame>
+      <Frame duration={10} >
+      <img src={C2} alt="2"/>       
+      </Frame>
+      <Frame duration={4500} >
+        <img src={C1} alt="1"/>    
+      </Frame>
+    </Keyframes>
+    
+  }
+}
+
+export default Animation
